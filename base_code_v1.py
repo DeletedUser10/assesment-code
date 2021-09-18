00f362a137fa2906819acea94af7fe5e4f09c543
DeletedUser9 <86692796+DeletedUser9@users.noreply.github.com>
2021-09-18 16:04:08 +1200

    Added labels for entries: customer name and recipt number

--- base_code_v1.py
+++ base_code_v1.py
@@ -22,6 +22,18 @@ def frame_1():
     entry_cus.grid(row= 2, column= 1, ipadx = 30)
     entry_rec = Entry(framel)
     entry_rec.grid(row=2, column=7, sticky= E)
+     #general information Labels
+    general = Label(framel, text = "General Information").grid(column=0, row = 0)
+    
+    #Glost Labels
+    ghost_label1 = Label(framel, text = "    " ).grid(row=1, column= 0 )
+    ghost_label3 = Label(framel, text = "    " ).grid(row=2, column= 2 )
+    ghost_label4 = Label(framel, text = "    " ).grid(row=2, column= 3 )
+    ghost_label5 = Label(framel, text = "    " ).grid(row=2, column= 4 )
+    
+    customer_name = Label(framel, text = "Customer Name").grid(column= 0, row= 2)
+    r_label = Label(framel, text = "Reciept Number ").grid(row = 2, column=6)
+    Label(framel, text="").grid(row = 3, column=0)
 
 def quit_button():
     quit_b = Button(root, text = "quit" , command = quit, padx = 30)
