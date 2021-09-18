230f968a8fafd258242c2fa8f151e935a405881f
DeletedUser9 <86692796+DeletedUser9@users.noreply.github.com>
2021-09-18 16:02:29 +1200

    added entry for customer name and reciept number

--- base_code_v1.py
+++ base_code_v1.py
@@ -17,6 +17,11 @@ def frame_1():
     global entry_cus, entry_rec, entry_item, entry_num
     framel = Frame(root, highlightbackground= 'grey', highlightthickness= 1)
     framel.grid(row= 1, column= 1, padx = 20, pady = 20, ipadx = 40, ipady= 30)
+     
+    entry_cus= Entry(framel)
+    entry_cus.grid(row= 2, column= 1, ipadx = 30)
+    entry_rec = Entry(framel)
+    entry_rec.grid(row=2, column=7, sticky= E)
 
 def quit_button():
     quit_b = Button(root, text = "quit" , command = quit, padx = 30)
