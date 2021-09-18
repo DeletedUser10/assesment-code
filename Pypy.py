96cac0e661ee978e0d5d2355c9bc3bd976ecacdd
DeletedUser9 <86692796+DeletedUser9@users.noreply.github.com>
2021-09-18 15:40:52 +1200

    Chamged window width

---
+++ Pypy.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+
+root = Tk()
+root.title('code')
+
+#Quits the program
+def quit():
+    root.destroy()
+
+def print_details():
+    return
+
+
+def frame_1():
+    global entry_cus, entry_rec, entry_item, entry_num
+    framel = Frame(root, highlightbackground= 'grey', highlightthickness= 1)
+    framel.grid(row= 1, column= 1, padx = 20, pady = 20, ipadx = 40, ipady= 30)
+
+def quit_button():
+    quit_b = Button(root, text = "quit" , command = quit, padx = 30)
+    quit_b.grid(column=1, row= 0, sticky= W ) 
+
+
+def main():
+    quit_button()
+    frame_1()
+    root.mainloop()
+
+
+main()
